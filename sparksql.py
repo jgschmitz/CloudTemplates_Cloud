30cdc8d8ce836ca696d35e32fd3bba58fc200c3a
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-12-31 09:45:47 -0600

    Update sparksql.py

--- sparksql.py
+++ sparksql.py
@@ -1,5 +1,15 @@
 %pyspark
 # install the pyspark driver pip3.7 install pyspark
+USAGE:
+      spark [-h] VALUE,...
+
+    EXAMPLES:
+      spark 1 5 22 13 53
+      ▁▁▃▂█
+      spark 0,30,55,80,33,150
+      ▁▂▃▄▂█
+      echo 9 13 5 17 1 | spark
+      ▄▆▂█▁
 
 """
 Example of loading .parquet formatted files into a in-memory SQLContext table and running SQL queries against it.
