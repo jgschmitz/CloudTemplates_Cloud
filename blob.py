799bb6fb44c73aa65c4e65f979bfe7d45d01c8ab
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-08-16 10:00:07 -0500

    Update blob.py

--- blob.py
+++ blob.py
@@ -3,7 +3,7 @@ python3
 #Blobstorage creation in Azure
 #For use in conjuction with Azure DevLab - you can also use AZ CLI
 #Jeff Schmitz 2019
-print 1,2,
+print 1,
 
 from azure.storage.blob import BlobService
 from os import listdir
