992994395a730e5bd23dbd42539155b2bf771fcd
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-10-06 07:55:13 -0500

    Update blob.py

--- blob.py
+++ blob.py
@@ -1,6 +1,6 @@
 #This is deprecated use the Azure CLI 
 #Blobstorage creation in Azure
-#For use in conjuction with Azure DevLab - you
+#For use in conjuction with Azure DevLa
 #Jeff Schmitz 2020
 
 from azure.storage.blob import BlobService
