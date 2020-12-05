4f4668c35b7b7c1d1eea3102474881020c74077b
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-12-05 08:49:38 -0600

    Update blob.py

--- blob.py
+++ blob.py
@@ -1,6 +1,6 @@
 #This is deprecated use the Azure CLI 
 #Blobstorage creation in Azure
-prin
+pr
 from azure.storage.blob import BlobService
 from os import listdir
 from os.path import isfile, join
