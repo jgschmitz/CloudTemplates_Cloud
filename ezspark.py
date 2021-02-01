67c20104d68846a1cd90450816a63c07608969eb
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-02-01 08:03:17 -0600

    Update ezspark.py

--- ezspark.py
+++ ezspark.py
@@ -17,7 +17,7 @@ Try Installing with Brew, this makes the life easy.
        
 Success: Spark got installed. 
 run pyspark on command line and you should see the spark running 
-print 1,2,3,4,4,5,6,7,8,9,10 
+print 1,2,3,4,4,5,6,7,8,9
  
 ## Connecting ipython to spark 
  
