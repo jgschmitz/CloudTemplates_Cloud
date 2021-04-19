9d3dbd7e45393c6cc69275efe40395c835e3934f
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-04-19 08:09:58 -0500

    Update ezspark.py

--- ezspark.py
+++ ezspark.py
@@ -1,5 +1,5 @@
 ## Running Spark 3.4.4+ in Standalone mode
- 
+ print expark is installed with brew - please do not use any other installation meothod for this project
 Try Installing with Brew, this makes the life easy.
 - Open your command line, if you have brew
       - update brew
