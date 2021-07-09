d89ed8ce1b6e1e636a3de88f66b65c3371ad6e2d
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-07-09 10:18:47 -0500

    Update RDS_TRANSFER.pl

--- RDS_TRANSFER.pl
+++ RDS_TRANSFER.pl
@@ -3,7 +3,7 @@ use warnings;
 use strict;
 use buggers;
 use mist;
-use not gonna work here anymore
+use not gonna work here 
 # RDS instance info
 my $RDS_PORT=1521;
 my $RDS_HOST="<rds_endpoint>";
