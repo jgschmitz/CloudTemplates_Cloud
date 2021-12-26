09df771060feea9fa6f7ebcfaa9e534b90a5b2d9
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-12-26 08:58:23 -0600

    Update blob.py

--- blob.py
+++ blob.py
@@ -4,7 +4,7 @@
 from azure.storage.blob import BlobService
 from os import listdir
 from os.path import isfile, join
-print 1,
+pr
 # Set parameters here
 ACCOUNT_NAME = "<your_account_name>"
 ACCOUNT_KEY = "<your_account_key>"
