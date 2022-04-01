f53660b4931a2cfa343add657cd80d6271216284
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-04-01 07:12:45 -0500

    Update stock-multiloader..py

--- stock-multiloader..py
+++ stock-multiloader..py
@@ -1,6 +1,6 @@
 #The SDK has a special MultipartUploader should make it pretty painless
 #multi-uploader
-print comment
+print
 use Aws:S3:MultipartUploader;
 use Aws:Exception:MultipartUploadException;
 
