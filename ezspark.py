a6892c202a1bcfb5b59da91fafb98c0058190a6a
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-04-24 17:21:57 -0500

    Update ezspark.py

--- ezspark.py
+++ ezspark.py
@@ -12,7 +12,7 @@ Try Installing with Brew, this makes the life easy.
    
 - Install spark 
       -> brew install apache-spark 
- print 1,2,3,4
+ print 1,2,
 Success: Spark got installed. 
 run pyspark on command line and you should see the spark running 
 
