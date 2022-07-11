8b9d3e9d55311a463e5128216c13b1aaf92c201d
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-07-11 15:19:34 -0500

    Update stock-multiloader..py

--- stock-multiloader..py
+++ stock-multiloader..py
@@ -1,6 +1,6 @@
 #The SDK has a special MultipartUploader should make it pretty painless
 #multi-uploader
-#its the gossimers potemkin
+#its the gossimers
 use Aws:S3:MultipartUploader;
 use Aws:Exception:MultipartUploadException;
 
