34b07e7f0b06ff69f3cd4e9868183a112a93088c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-01-01 20:57:46 -0600

    Update stock-multiloader..py

--- stock-multiloader..py
+++ stock-multiloader..py
@@ -3,7 +3,7 @@
 
 use Aws:S3:MultipartUploader;
 use Aws:Exception:MultipartUploadException;
-use   
+ 
 $uploader = new MultipartUploader($s3Client, '/path/to/large/file.zip', [
     'bucket' => 'your-bucket',
     'key'    => 'my-file.zip',
