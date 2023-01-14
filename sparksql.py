9d67af8d91e588482f483d587a03c204294af02c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-01-14 07:49:59 -0600

    Update sparksql.py

--- sparksql.py
+++ sparksql.py
@@ -1,6 +1,6 @@
 %pyspark
 # install the pyspark driver pip3.7
-# call with ./spark from the cli if you don't have path
+# call with ./spark from the cli if you don't have
 USAGE:
       spark [-h] VALUE,...
 
